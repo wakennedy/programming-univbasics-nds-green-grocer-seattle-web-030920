1e2ece6dd55bcbb1060373ddd5be8fe71a15a3e3
William Kennedy <alexkenned@gmail.com>
2020-02-26 23:59:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,11 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  i = 0 
+  while i < collection.length do 
+    if name == 
+  
+  
 end
 
 def consolidate_cart(cart)
