89eac7291c0abab43771c23e090e3617003bdf69
William Kennedy <alexkenned@gmail.com>
2020-02-27 00:44:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -59,7 +59,7 @@ def apply_clearance(cart)
   i = 0 
   while i < cart.length do 
     if cart[i][:clearance]
-      cart[i][:price] = (cart[i][:price] - )
+      cart[i][:price] = (cart[i][:price] - (cart[i][:price] 8 0.20)).round(2))
 end
 
 def checkout(cart, coupons)
