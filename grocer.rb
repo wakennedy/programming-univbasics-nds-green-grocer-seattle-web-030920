c1f46573bb70b77aa442414e16121c1dc4f351a0
William Kennedy <alexkenned@gmail.com>
2020-02-27 00:12:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,7 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   i = 0 
   while i < collection.length do 
     if collection[i][:item] == name
-      return collection[i][:item]
+      return collection[i]
     end
     i += 1 
   end
