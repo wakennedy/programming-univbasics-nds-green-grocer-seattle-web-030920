f64a7a640312d04398c222fa0195771ba401335d
William Kennedy <alexkenned@gmail.com>
2020-02-27 00:01:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,8 +4,8 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   i = 0 
   while i < collection.length do 
-    if name == collection[i]
-      return collection[i]
+    if name == collection[i][:item]
+      puts collection[i][:item]
     else return nil 
     end
     i += 1 
